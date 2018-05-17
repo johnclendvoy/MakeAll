1733f72f622cb95406c19d17c25186c11f25d63a
John C. Lendvoy <john@Kristas-MacBook-Pro.local>
2018-05-16 20:52:43 -0600

    remove rewriteRules.php

--- rewriteRules.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Available placeholders: johnclendvoy, MakeAll, johnclendvoy, makeall
-return [
-    'src/MyPackage.php' => 'src/MakeAll.php',
-    'config/mypackage.php' => 'config/makeall.php',
-    'src/Facades/MyPackage.php' => 'src/Facades/MakeAll.php',
-    'src/MyPackageServiceProvider.php' => 'src/MakeAllServiceProvider.php',
-];
